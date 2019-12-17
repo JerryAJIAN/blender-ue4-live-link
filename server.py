5e8b7043ef967419ff95ee52c361feb0afd478ec
Tom Delaney <delaneyt1@gmail.com>
2019-12-17 10:43:32 -0500

    :art: Clean up server.py  based on linter

--- server.py
+++ server.py
@@ -1,5 +1,32 @@
 import socket
 import threading
+import bpy
+from bpy.props import (BoolProperty, IntProperty, PointerProperty)
+from bpy.types import (PropertyGroup)
+
+
+class MySettings(PropertyGroup):
+
+    is_running: BoolProperty(
+        name="is_running",
+        description="Is the server currently running",
+        default=False)
+
+    port: IntProperty(
+        name="port",
+        description="Port to listen for livelink connections on",
+        default=8888,
+        min=8080)
+
+
+def register():
+    bpy.utils.register_class(MySettings)
+
+
+def unregister():
+    bpy.utils.unregister_class(MySettings)
+    bpy.types.Scene.my_tool = PointerProperty(type=MySettings)
+
 
 def startserver():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,5 +38,9 @@ def startserver():
         print(f"Connection from {address} has been established!")
         clientsocket.send(bytes("welcome to the server", "utf-8"))
 
-thread = threading.Thread(target=startserver)
-thread.start()
+    thread = threading.Thread(target=startserver)
+    thread.start()
+
+
+register()
+print("Registered")
