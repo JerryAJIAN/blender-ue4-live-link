5df27e12c996827a95802f598b51463935934b76
Tom Delaney <delaneyt1@gmail.com>
2019-12-18 09:54:08 -0500

    clean up packaging

--- package.sh
+++ package.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-zip UE4LiveLink.zip UE4LiveLink
-zip UE4LiveLink.zip README.md
-zip UE4LiveLink.zip LICENSE
+cp -R src UE4LiveLink
+cp README.md UE4LiveLink/
+cp LICENSE UE4LiveLink/
+zip -r UE4LiveLink.zip UE4LiveLink
+rm -rf UE4LiveLink
+
