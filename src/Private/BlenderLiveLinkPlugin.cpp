96d3522c33b14664402bd03d16899f2ac3f254a7
Tom Delaney <delaneyt1@gmail.com>
2019-12-20 00:03:26 -0500

    compiles and loads in python

--- src/Private/BlenderLiveLinkPlugin.cpp
+++ src/Private/BlenderLiveLinkPlugin.cpp
@@ -1,17 +1,18 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/* This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
 
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   General Public License for more details.
 
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 #include "BlenderLiveLinkPlugin.h"
 
-#include "RequiredProgramMainCPPInclude.h"
-
-DEFINE_LOG_CATEGORY_STATIC(LogBlenderLiveLinkPlugin, Log, All);
-
-IMPLEMENT_APPLICATION(BlenderLiveLinkPlugin, "BlenderLiveLinkPlugin");
-
-INT32_MAIN_INT32_ARGC_TCHAR_ARGV()
+int tom_sum(int x, int y)
 {
-	GEngineLoop.PreInit(ArgC, ArgV);
-	UE_LOG(LogBlenderLiveLinkPlugin, Display, TEXT("Hello World"));
-	return 0;
+	return x + y;
 }
